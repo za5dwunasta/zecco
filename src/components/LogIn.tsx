2369ecbe382e568bf854a23c0705de23fb1c7b42
za5dwunasta <magdalena.motyl@gmail.com>
2021-01-19 13:29:23 +0000

    final cleanings #2

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -4,7 +4,7 @@ import Button from "./Button"
 const LogIn: React.FC = () => {
   return (
     <div className="login">
-      <Button text="Log in" dropdownList={["new item", "another"]} />
+      <Button text="Log in" />
       <Button text="Create account" additionalStyle="login__button" />
     </div>
   )
